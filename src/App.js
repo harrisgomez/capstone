36813dfe22e332c93c85a48962923e3b9a4ea9be
harrisgomez <node.harris@gmail.com>
2023-06-16 13:24:31 -0400

    ADD: nav bar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 
 import Home from "./routes/home/home.component";
+
+const Navigation = () => {
+    return (
+        <div>
+            <div>
+                <h1>Nav Bar</h1>
+            </div>
+            <Outlet />
+        </div>
+    );
+};
 
 const Shop = () => {
     return <h1>Shop Page</h1>
@@ -9,11 +20,12 @@ const Shop = () => {
 const App = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home />}>
+            <Route path='/' element={<Navigation />}>
+                <Route index element={<Home />} />
                 <Route path='shop' element={<Shop />} />
             </Route>
         </Routes>
-    )
+    );
 };
 
 export default App;
