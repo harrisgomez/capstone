1a68cc72847b6eefb4586f6808e15929e832b834
harrisgomez <node.harris@gmail.com>
2023-06-27 15:52:18 -0400

    ADD: firebase config, signin with google popup, retrieve access_token

---
+++ src/utils/firebase/firebase.utils.js
@@ -0,0 +1,26 @@
+import { initializeApp } from 'firebase/app';
+import {
+    getAuth,
+    signInWithRedirect,
+    signInWithPopup,
+    GoogleAuthProvider
+} from 'firebase/auth';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyC8-34H0lhB2N0rBV-GBFs1YcsDZqdxSu8",
+    authDomain: "capstone-db-e0f43.firebaseapp.com",
+    projectId: "capstone-db-e0f43",
+    storageBucket: "capstone-db-e0f43.appspot.com",
+    messagingSenderId: "341363822874",
+    appId: "1:341363822874:web:11c13fe1b80e54989c9ca3"
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+provider.setCustomParameters({
+    prompt: 'select_account'
+});
+
+export const auth = getAuth();
+export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
