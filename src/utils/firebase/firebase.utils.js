bab9972f3edc81879b8b5aa91dc9b6d977817067
harrisgomez <node.harris@gmail.com>
2023-10-05 16:11:56 -0400

    ADD: get products and categories from firestore

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -15,7 +15,9 @@ import {
     getDoc,
     setDoc,
     collection,
-    writeBatch
+    writeBatch,
+    query,
+    getDocs
 } from 'firebase/firestore';
 
 const firebaseConfig = {
@@ -50,6 +52,21 @@ export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => 
     });
 
     await batch.commit();
+};
+
+export const getCategoriesAndDocuments = async () => {
+    const collectionRef = collection(db, 'categories');
+    const q = query(collectionRef);
+    const querySnapshot = await getDocs(q);
+    const categoryMap = querySnapshot.docs.reduce((acc, docSnapshot) => {
+        const { title, items } = docSnapshot.data();
+
+        acc[title.toLowerCase()] = items;
+
+        return acc;
+    }, {});
+
+    return categoryMap;
 };
 
 export const createUserDocFromAuth = async (userAuth, additionalInformation = {}) => {
