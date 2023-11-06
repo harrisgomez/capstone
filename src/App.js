18e499a09785b833b7dcb53c6a3db9f86a8c0bae
Harris Gomez <node.harris@gmail.com>
2023-11-06 17:21:56 -0500

    ADD: create user reducer; EDIT: corrected compose, createStore, applyMiddleware imports

--- src/App.js
+++ src/App.js
@@ -1,4 +1,8 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
 import { Routes, Route } from 'react-router-dom';
+
+import { onAuthStateChangedListener, createUserDocFromAuth } from "./utils/firebase/firebase.utils";
 
 import Navigation from './routes/navigation/navigation.component';
 import Home from "./routes/home/home.component";
@@ -6,7 +10,22 @@ import Authentication from './routes/authentication/authentication.component';
 import Shop from './routes/shop/shop.component';
 import Checkout from './routes/checkout/checkout.component';
 
+import { setCurrentUser } from "./store/user/user.action";
+
 const App = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChangedListener(user => {
+            if (user) {
+                createUserDocFromAuth(user);
+            }
+            dispatch(setCurrentUser(user));
+        });
+
+        return unsubscribe;
+    }, [dispatch]);
+
     return (
         <Routes>
             <Route path='/' element={<Navigation />}>
