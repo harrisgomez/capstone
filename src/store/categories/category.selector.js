95e1cf48e86b697e37d24ad95c6faaa67614117e
Harris Gomez <node.harris@gmail.com>
2023-11-08 21:44:56 -0500

    ADD: reselect pkg, memoized category selector

--- src/store/categories/category.selector.js
+++ src/store/categories/category.selector.js
@@ -1,10 +1,19 @@
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = state => state.categories;
+
+export const selectCategories = createSelector(
+    [selectCategoryReducer],
+    categoriesSlice => categoriesSlice.categories
+)
+
 // For variables that may be re-usable
 // Selectors handle the business logic
-export const selectCategoriesMap = state => {
-    console.log('selector fired');
-    return state.categories.categories.reduce((acc, category) => {
+export const selectCategoriesMap = createSelector(
+    [selectCategories],
+    categories => categories.reduce((acc, category) => {
         const { title, items } = category;
         acc[title.toLowerCase()] = items;
         return acc;
-    }, {});
-};
+    }, {})
+);
