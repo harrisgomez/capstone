acdcf0f56c963ed7dbbfba1ce36219eed1a110ee
Harris Gomez <node.harris@gmail.com>
2023-11-14 14:59:02 -0500

    ADD: redux-thunk pt.2

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -5,20 +5,13 @@ import { useDispatch } from 'react-redux';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
 
-import { setCategories } from '../../store/categories/category.action';
-
-import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
+import { fetchCategoriesAsync } from '../../store/categories/category.action';
 
 const Shop = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        const getCategoriesMap = async () => {
-            const categoriesArr = await getCategoriesAndDocuments();
-            dispatch(setCategories(categoriesArr));
-        };
-
-        getCategoriesMap();
+        dispatch(fetchCategoriesAsync());
     }, [dispatch]);
 
     return (
--- src/store/categories/category.action.js
+++ src/store/categories/category.action.js
@@ -1,5 +1,32 @@
 import { CATEGORIES_ACTION_TYPES } from "./category.types";
+
 import { createAction } from "../../utils/reducer/reducer.utils";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
 
 export const setCategories = categoriesArr =>
     createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categoriesArr);
+
+export const fetchCategoriesStart = () =>
+    createAction(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = categoriesArr =>
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS,
+        categoriesArr
+    );
+
+export const fetchCategoriesFailed = error =>
+    createAction(
+        CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED,
+        error
+    );
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+    dispatch(fetchCategoriesStart());
+    try {
+        const categoriesArr = await getCategoriesAndDocuments('categories');
+        dispatch(fetchCategoriesSuccess(categoriesArr));
+    } catch (error) {
+        dispatch(fetchCategoriesFailed(error));
+    }
+};
