2d65f08f090e07c170d846921f246ae874d938c7
Harris Gomez <node.harris@gmail.com>
2023-11-14 14:30:27 -0500

    ADD: redux-thunk pkg; UPDATE: category reducer

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, createStore, applyMiddleware } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import { rootReducer } from './root-reducer';
 
@@ -14,7 +15,10 @@ const persistConfig = {
 const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 // middlwares 'enhance' the store by catching actions before they hit the store
-const middleWares = [process.env.NODE_ENV !== 'production' && logger].filter(Boolean);
+const middleWares = [
+    process.env.NODE_ENV !== 'production' && logger,
+    thunk
+].filter(Boolean);
 
 // This condition uses an enhanced devtools compose if we're not in production, there is a windows object, and devtools (chrome) exists
 const composeEnhancer = (process.env.NODE_ENV !== 'production'
